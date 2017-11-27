a4d2559ea3b7d3808114aa3c5f71123371ca76a9
Max Hodges <Max@whiterabbitjapan.com>
2017-11-28 03:22:26 +0900

    add to git

---
+++ src/stories/index.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import { storiesOf } from "@storybook/vue";
+import MyButton from "./Button.vue";
+
+storiesOf("MyButton", module)
+  .add(
+    "story as a template",
+    () => '<my-button :rounded="true">story as a function template</my-button>'
+  )
+  .add("story as a component", () => ({
+    components: { MyButton },
+    template: '<my-button :rounded="true">rounded</my-button>'
+  }))
+  .add("normal button", () => ({
+    components: { MyButton },
+    template: '<my-button :rounded="false">normal</my-button>'
+  }));
