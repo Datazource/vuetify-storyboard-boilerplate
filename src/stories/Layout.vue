9f8c6d512cad0bebb543e773b7de6a06d80f27f0
Kael <kaelwd@gmail.com>
2017-12-05 23:16:37 +1100

    Use valid HTML in templates

--- src/stories/Layout.vue
+++ src/stories/Layout.vue
@@ -1,11 +1,4 @@
 <template>
-<html>
-  <head>
-    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet"/>
-    <link href="https://unpkg.com/vuetify/dist/vuetify.min.css" rel="stylesheet"/>
-  </head>
-  <body>
-
   <v-app id="inspire" dark>
     <v-navigation-drawer
       clipped
@@ -32,50 +25,45 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+
     <v-toolbar app fixed clipped-left>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
       <v-toolbar-title>Application</v-toolbar-title>
     </v-toolbar>
+
     <v-content>
-        <v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
-      <v-flex xs12>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">                      <slot/>
-</v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout></v-container>
-
-      <v-container fluid fill-height>
-        <v-layout justify-center align-center>
-          <v-tooltip right>
-            <v-btn icon large :href="source" target="_blank" slot="activator">
-              <v-icon large>code</v-icon>
-            </v-btn>
-            <span>Source</span>
-          </v-tooltip>
+      <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-card dark color="primary">
+              <v-card-text class="px-0">
+                <slot/>
+              </v-card-text>
+            </v-card>
+          </v-flex>
         </v-layout>
-
       </v-container>
     </v-content>
+
     <v-footer app fixed>
       <span>&copy; 2017</span>
     </v-footer>
   </v-app>
-  </body>
-</html>
 </template>
 
 <script>
-export default {
-  name: "layout",
+  export default {
+    name: 'layout',
 
-  data: () => ({
-    drawer: null
-  }),
-  props: {
-    source: String
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
   }
-};
 </script>
+
+<style type="text/css">
+  @import "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons";
+</style>
