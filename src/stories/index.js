b8c874bc93ab36afdb7919fabd346175cc982f64
Max Hodges <Max@whiterabbitjapan.com>
2017-12-05 22:11:01 +0900

    pass props for menu items

--- src/stories/index.js
+++ src/stories/index.js
@@ -12,6 +12,82 @@ storiesOf("Welcome", module).add("to Storybook", () => ({
   methods: { action: linkTo("Button") }
 }));
 
+const menuItems = [
+  {
+    action: "local_activity",
+    title: "Attractions",
+    items: [{ title: "List Item" }]
+  },
+  {
+    action: "restaurant",
+    title: "Dining",
+    active: true,
+    items: [
+      { title: "Breakfast & brunch" },
+      { title: "New American" },
+      { title: "Sushi" }
+    ]
+  },
+  {
+    action: "school",
+    title: "Education",
+    items: [{ title: "List Item" }]
+  },
+  {
+    action: "directions_run",
+    title: "Family",
+    items: [{ title: "List Item" }]
+  },
+  {
+    action: "healing",
+    title: "Health",
+    items: [{ title: "List Item" }]
+  },
+  {
+    action: "content_cut",
+    title: "Office",
+    items: [{ title: "List Item" }]
+  },
+  {
+    action: "local_offer",
+    title: "Promotions",
+    items: [{ title: "List Item" }]
+  }
+];
+
+const menuItemsAlt = [
+  {
+    action: "inbox",
+    title: "Mailbox"
+  },
+  {
+    action: "note",
+    title: "My Address"
+  },
+  {
+    action: "assignment",
+    title: "In Progress",
+    active: true,
+    items: [
+      { title: "Consolidation" },
+      { title: "Repacking" },
+      { title: "Shipping" }
+    ]
+  },
+  {
+    action: "playlist_add_check",
+    title: "Registration"
+  },
+  {
+    action: "account_circle",
+    title: "Account"
+  },
+  {
+    action: "view_headline",
+    title: "Shipped"
+  }
+];
+
 storiesOf("Button", module)
   .add("button template", () => ({
     template: '<my-button :rounded="true">round</my-button>'
@@ -28,11 +104,21 @@ storiesOf("Button", module)
 storiesOf("Layout", module)
   .add("with button", () => ({
     components: { Layout, MyButton },
+    data() {
+      return {
+        items: menuItems
+      };
+    },
     template:
-      '<layout><my-button :rounded="false">my button</my-button></layout>'
+      '<layout :items="items"><my-button :rounded="false">my button</my-button></layout>'
   }))
-  .add("with button rounded", () => ({
+  .add("with rounded button", () => ({
     components: { Layout, MyButton },
+    data() {
+      return {
+        items: menuItemsAlt
+      };
+    },
     template:
-      '<layout><my-button :rounded="true">my button</my-button></layout>'
+      '<layout :items="items"><my-button :rounded="true">my button</my-button></layout>'
   }));
